99b8546513d480c1a376c7f1a4817b4abc632d0b
Gustavo Marin <wstov@hotmail.com>
2023-11-19 22:58:39 -0600

    Vistas y Edit
    Se finaliza los ajustes de la sección de editar y se ajustan secciones de la interfaz grafica

--- libreria/src/main/java/com/libreria/application/service/ProductService.java
+++ libreria/src/main/java/com/libreria/application/service/ProductService.java
@@ -27,13 +27,24 @@ public class ProductService {
         return productRepository.getProductById(id);
     }
 
-    public Product saveProduct(Product product){
-        User user = new User();
-        user.setId(1);
-        product.setDateCreated(LocalDateTime.now());
-        product.setDateUpdated(LocalDateTime.now());
-        product.setUser(user);
-        return  productRepository.saveProduct(product);
+//    GUARDAR Y ACTUALIZAR LA INFORMACION DE LOS LIBROS
+    public Product saveProduct(Product product) {
+        if (product.getId() == null) {
+            User user = new User();
+            user.setId(1);
+            product.setDateCreated(LocalDateTime.now());
+            product.setDateUpdated(LocalDateTime.now());
+            product.setUser(user);
+            return productRepository.saveProduct(product);
+        
+        }else{
+            Product productDB= productRepository.getProductById(product.getId());
+            product.setCode(productDB.getCode());
+            product.setUser(productDB.getUser());
+            product.setDateCreated(productDB.getDateCreated());
+            product.setDateUpdated(LocalDateTime.now());
+            return productRepository.saveProduct(product);
+        }
     }
 
     public void deleteProductById(Integer id){
